ba31151336428f0fb25bc0570caad034d76c733b
koffi eric <your@email.com>
2021-07-06 15:05:45 -0400

    Ajout code2 Etape1

--- Etape01_Etudiant01_Creation_Tables.sql
+++ Etape01_Etudiant01_Creation_Tables.sql
@@ -43,3 +43,66 @@ id_fabricant int unsigned,
 FOREIGN KEY(id_fabricant) REFERENCES fabricant(id_fabricant) ON DELETE no action on update no action,
 FOREIGN KEY(id_poste) REFERENCES poste (id_poste) ON DELETE no action on update no action
 )engine=innodb;
+
+CREATE TABLE IF NOT EXISTS usager (
+  id_usager INT UNSIGNED PRIMARY KEY,
+  nom_usager VARCHAR(45) NOT NULL,
+  prenom_usager VARCHAR(45) NOT NULL,
+  telephone_usager VARCHAR(15) NULL,
+  local_id_local INT UNSIGNED NULL,
+  
+    FOREIGN KEY (local_id_local)
+    REFERENCES local (id_local)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS editeur (
+  id_editeur INT UNSIGNED  PRIMARY KEY ,
+  nom_editeur VARCHAR(45) NOT NULL)
+ENGINE = InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS logiciel (
+  id_logiciel INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nom_logiciel VARCHAR(45) NOT NULL,
+  nb_licence INT NULL,
+  id_editeur INT UNSIGNED NOT NULL,
+    FOREIGN KEY (id_editeur)
+    REFERENCES editeur (id_editeur)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS poste_has_logiciel (
+  id_poste INT UNSIGNED NOT NULL,
+  id_logiciel INT UNSIGNED NOT NULL,
+  PRIMARY KEY (id_poste, id_logiciel),
+    FOREIGN KEY (id_poste)
+    REFERENCES poste (id_poste)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    FOREIGN KEY (id_logiciel)
+    REFERENCES logiciel (id_logiciel)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS usager_has_poste (
+  id_usager INT UNSIGNED NOT NULL,
+  id_poste INT UNSIGNED NOT NULL,
+  PRIMARY KEY (id_usager, id_poste),
+    FOREIGN KEY (id_usager)
+    REFERENCES usager (id_usager)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    FOREIGN KEY (id_poste)
+    REFERENCES poste (id_poste)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
