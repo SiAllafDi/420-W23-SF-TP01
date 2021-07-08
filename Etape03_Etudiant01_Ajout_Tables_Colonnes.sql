402658be897ccaa559d4a8f593c927d86f166512
unknown <sidi.sidahmed23@gmail.com>
2021-07-08 00:58:04 -0400

    Ajout Etape3 Etudiant1

--- Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
@@ -1,6 +1,6 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
+ --> Sidi Allaf
  Nom de la deuxième personne
  
  Date  JJ-MM-AAAA                                             
@@ -10,3 +10,15 @@ USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+create table if not exists typeusager(
+id_typeUsager int unsigned primary key,
+nom_statut varchar(45) null 
+
+)engine=innodb;
+alter table usager 
+add column id_typeUsager int unsigned,
+add constraint Fk_id_typeUsager foreign key (id_typeUsager) REFERENCES typeusager(id_typeUsager) ON DELETE no action on update no action;
+select * from typeusager;
+INSERT INTO `posteclientdfc`.`typeusager` (`id_typeUsager`, `nom_statut`) VALUES ('1', 'Professeur');
+INSERT INTO `posteclientdfc`.`typeusager` (`id_typeUsager`, `nom_statut`) VALUES ('2', 'Employé');
+INSERT INTO `posteclientdfc`.`typeusager` (`id_typeUsager`, `nom_statut`) VALUES ('3', 'Étudiant');
